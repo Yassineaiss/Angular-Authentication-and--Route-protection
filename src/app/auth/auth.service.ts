0d379bec57420aa458d48b46618bf9c78b99da76
Yassine Aissaoui <Yassaiss@gmail.com>
2022-08-17 12:56:32 +0200

    Login error Handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError } from "rxjs/operators";
 import { throwError } from "rxjs";
- export interface AuthResponseData {
+export interface AuthResponseData {
   idToken: string;
   email: string;
   refreshToken: string;
@@ -24,28 +24,37 @@ export class AuthService {
           returnSecureToken: true,
         }
       )
-      .pipe(catchError(errorRes => {
-        let errorMessage = 'An unknown error occured!' ;
-        if(!errorRes.error|| !errorRes.error.error){
-          return throwError(errorMessage)
-        }
-        switch(errorRes.error.error.message){
-          case 'EMAIL_EXISTS':
-            errorMessage= 'this email exists already' ;
-        }
-        return throwError(errorMessage);
-      }));
+      .pipe(catchError(this.handleError));
   }
-  login(email:string, password:string){
-     return this.http.post<AuthResponseData>(
-      'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyBFl_CobruT60XhVcp7Virs8oNnyhVJ0A8',
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyBFl_CobruT60XhVcp7Virs8oNnyhVJ0A8",
 
         {
           email: email,
           password: password,
           returnSecureToken: true,
         }
-
       )
+      .pipe(catchError(this.handleError));
+  }
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = "An unknown error occured!";
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case "EMAIL_EXISTS":
+        errorMessage = "this email exists already";
+        break;
+      case "EMAIL_NOT_FOUND":
+        errorMessage = "this email does not exist ";
+        break;
+      case "INVALID_PASSWORD":
+        errorMessage = "this password is not correct";
+        break;
+    }
+    return throwError(errorMessage);
   }
 }
